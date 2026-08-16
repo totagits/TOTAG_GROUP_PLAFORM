912898872ca2e3c8193aa9b3d65c0107964fcd11
totagits <tis@totaggroup.com>
2026-08-16 08:43:48 -0500

    Fix createRoot import source in main.tsx from react to react-dom/client

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,4 +1,4 @@
-import { createRoot } from "react";
+import { createRoot } from "react-dom/client";
 import App from "./App";
 import { ErrorBoundary } from "@/components/error-boundary";
 import "./index.css";
